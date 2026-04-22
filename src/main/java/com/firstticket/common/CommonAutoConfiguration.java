4d9a36d183720c9820e0ee6099193fb6ddaadb34
haeun228 <123haeun@naver.com>
2026-04-22 19:21:29 +0900

    feat: CustomPageableResolver 추가

--- src/main/java/com/firstticket/common/CommonAutoConfiguration.java
+++ src/main/java/com/firstticket/common/CommonAutoConfiguration.java
@@ -2,13 +2,15 @@ package com.firstticket.common;
 
 import com.firstticket.common.exception.GlobalExceptionHandler;
 import com.firstticket.common.persistence.JpaConfig;
+import com.firstticket.common.web.WebConfig;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
 @Import({
         GlobalExceptionHandler.class,
-        JpaConfig.class
+        JpaConfig.class,
+        WebConfig.class
 })
 public class CommonAutoConfiguration {
 }
