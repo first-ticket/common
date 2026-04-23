d976c3add629f1610509b5e57cd9f0d88bebc270
haeun228 <123haeun@naver.com>
2026-04-24 03:03:20 +0900

    feat: JsonUtil 구현

---
+++ src/main/java/com/firstticket/common/json/JsonUtil.java
@@ -0,0 +1,45 @@
+package com.firstticket.common.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Slf4j
+public class JsonUtil {
+
+    private static ObjectMapper objectMapper;
+
+    @Autowired
+    public void init(ObjectMapper objectMapper) {
+        JsonUtil.objectMapper = objectMapper;
+    }
+
+    public static String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.error("[JSON] 직렬화 실패: {}", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    public static <T> T fromJson(String json, Class<T> type) {
+        try {
+            return objectMapper.readValue(json, type);
+        } catch (JsonProcessingException e) {
+            log.error("[JSON] 역직렬화 실패: {}", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    public static <T> T fromJson(String json, TypeReference<T> typeReference) {
+        try {
+            return objectMapper.readValue(json, typeReference);
+        } catch (JsonProcessingException e) {
+            log.error("[JSON] 역직렬화 실패: {}", e.getMessage(), e);
+            return null;
+        }
+    }
+}
