24aa6e8313c2a4ff2a209c75a83d8b7b9a4cf06a
조하은 <137027947+haeun228@users.noreply.github.com>
2026-04-27 15:57:58 +0900

    [chore][common] common 모듈 JPA 의존성 분리 (#17)

--- src/main/java/com/firstticket/common/CommonAutoConfiguration.java
+++ src/main/java/com/firstticket/common/CommonAutoConfiguration.java
@@ -3,7 +3,6 @@ package com.firstticket.common;
 import com.firstticket.common.exception.GlobalExceptionHandler;
 import com.firstticket.common.feign.FeignConfig;
 import com.firstticket.common.json.JsonConfig;
-import com.firstticket.common.persistence.JpaConfig;
 import com.firstticket.common.web.WebConfig;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.Import;
 @AutoConfiguration
 @Import({
         GlobalExceptionHandler.class,
-        JpaConfig.class,
         WebConfig.class,
         FeignConfig.class,
         JsonConfig.class
